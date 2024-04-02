b2a64b953241b22773802ae2a94dfafe15021fc0
David Ward <davidmward@mail.com>
2024-04-02 00:54:54 -0700

    comment on style

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
+ * Coding Style:
+ * Inspired by K&R and linux kernel standards.
+ * Functions use snake_case.
+ * Variables use short names and snake_case when needed.
+ * Block curly brace goes on the same line, except for funtions.
+ */
+
 int random_int(int range, int min)
 {
         int value = rand() % range + min;
