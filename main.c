ab7cc89761193d639a51eacefb96737021ba0341
David Ward <davidmward@mail.com>
2024-04-02 00:48:43 -0700

    initial main and random number

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int random_int(int range, int min)
+{
+        int value = rand() % range + min;
+        return value;
+}
 
 int main()
 {
-    return 0;
+        // seed random function
+        srand(time(NULL));
+
+        return 0;
 }
