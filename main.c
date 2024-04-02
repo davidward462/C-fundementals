a83797195c8ef866cd8a73323c8680522de1e95a
David Ward <davidmward@mail.com>
2024-04-02 16:54:33 -0700

    add and test abs()

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 /*
  * Coding Style:
@@ -10,6 +11,14 @@
  * Block curly brace goes on the same line, except for funtions.
  */
 
+int abs(int value)
+{
+        if (value < 0) {
+                value = value * -1;
+        }
+        return value;
+}
+
 // Output a random integer x in the range min <= x <= max.
 int rand_interval(int min, int max)
 {
@@ -18,16 +27,41 @@ int rand_interval(int min, int max)
         return value;
 }
 
+// Unit tests
+// TODO: maybe move these to another file at some point.
+
+void test_abs()
+{
+        bool pass = false;
+        int expected = 5;
+        int actual = abs(-5);
+
+        if (actual == expected) {
+                printf("TEST PASSED.\n");
+        }
+        else {
+                printf("TEST FAILED.\n");
+        }
+}
+
+void test_rand_interval(int count, int min, int max)
+{
+        printf("Displaying %d calls of rand_interval(%d, %d)\n", count, min, max);
+        
+        for (int i = 0; i < count; i++) {
+                int value = rand_interval(min, max);
+                printf("%d\n", value);
+        }
+}
+
 int main()
 {
         // seed random function
         srand(time(NULL));
 
         // for testing
-        for (int i = 0; i < 15; i++) {
-                int value = rand_interval(0, 1);
-                printf("%d\n", value);
-        }
+        test_rand_interval(20, -10, 10);
+        test_abs();
 
         return 0;
 }
